1fd3440a69a4239c7fa2ceca659243eb8e428364
DmitryF17 <farodmin@yandex.ru>
2020-03-29 15:47:05 +0300

    3. Шаблонизатор.[#259777]

--- chapter_001/src/test/java/template/TemplateTest.java
+++ chapter_001/src/test/java/template/TemplateTest.java
@@ -1,40 +1,44 @@
 package template;
 
-import org.junit.Test;
-import ru.job4j.template.GeneratorR;
-import ru.job4j.template.NameSubj;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.Is.is;
+import java.util.NoSuchElementException;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.template.GeneratorR;
 
 public class TemplateTest {
+    public TemplateTest() {
+    }
+
     @Test
     public void templTest() {
         String temp = "I am ${name}, Who are ${subject}?";
-        Map<String, Object> res = new HashMap<>();
-        NameSubj o = new NameSubj("you");
-        res.put("Dmitrii", o);
-        assertThat(new GeneratorR().produce(temp, res), is("I am Dmitrii, Who are you"));
+        Map<String, Object> res = new HashMap();
+        res.put("name", "Dmitrii");
+        res.put("subject", "you");
+        Assert.assertThat((new GeneratorR()).produce(temp, res), Is.is("I am Dmitrii, Who are you"));
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test(
+            expected = NoSuchElementException.class
+    )
     public void whenNotKey() {
         String temp = "I am ${name}, Who are ${subject}?";
-        Map<String, Object> res = new HashMap<>();
-        NameSubj o = new NameSubj("you");
-        res.put(null, o);
-        String result = new GeneratorR().produce(temp, res);
+        Map<String, Object> res = new HashMap();
+        res.put(null, "Oleg");
+        (new GeneratorR()).produce(temp, res);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(
+            expected = IllegalArgumentException.class
+    )
     public void whenNotTemp() {
         String temp = "I am , Who are ${subject}?";
-        Map<String, Object> res = new HashMap<>();
-        NameSubj o = new NameSubj("you");
-        res.put("Dmitry", o);
-        String result = new GeneratorR().produce(temp, res);
+        Map<String, Object> res = new HashMap();
+        res.put("name", "Petr");
+        res.put("subject", "you");
+        (new GeneratorR()).produce(temp, res);
     }
 }
