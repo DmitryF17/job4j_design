2c613e2743746fe230555deb445924c5e4523747
DmitryF17 <farodmin@yandex.ru>
2020-04-09 12:09:52 +0300

    0.3. Буферизированные потоки.[#259758]

--- chapter_001/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_001/src/main/java/ru/job4j/io/LogFilter.java
@@ -3,7 +3,6 @@ package ru.job4j.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,23 +13,28 @@ import java.util.List;
  * version 01.
  * @since 01.04.2020.
  */
-
 public class LogFilter {
-    public List<String> lines;
-
     /**
      * filter for the second from the end.
      *
      * @param file
      * @return null.
      */
-
     public static List<String> filter(String file) {
-        String[] lin = file.split(" ");
-        if (lin[lin.length - 2].equals("404")) {
-            return Collections.singletonList(file);
+        List<String> lines = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines().forEach(lines::add);
+            for (String line : lines) {
+                String[] lin = line.split(" ");
+                if (lin[lin.length - 2].equals("404")) {
+                    res.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        return res;
     }
 
     /**
@@ -39,22 +43,16 @@ public class LogFilter {
      *
      * @param args
      */
-
     public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
-            List<String> lines = new ArrayList<>();
-            in.lines().forEach(lines::add);
-            for (String line : lines) {
-                List<String> log = filter(line);
-                if (log != null) {
-                    System.out.println(log);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        List<String> log = filter("log.txt");
+        System.out.println(log);
     }
 }
+
+
+
+
+
 
 
 
