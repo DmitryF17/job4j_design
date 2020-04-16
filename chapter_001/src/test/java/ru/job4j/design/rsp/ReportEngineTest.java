262992d59e3795b5be2b75e5e79d0c77ec6b67b6
DmitryF17 <farodmin@yandex.ru>
2020-04-16 16:08:32 +0300

    check

--- chapter_001/src/test/java/ru/job4j/design/rsp/ReportEngineTest.java
+++ chapter_001/src/test/java/ru/job4j/design/rsp/ReportEngineTest.java
@@ -25,24 +25,4 @@ public class ReportEngineTest {
                 .append(worker.getSalary()).append(";");
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
-
-    @Test
-    public void whenHRGenerated() {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employer worker = new Employer("Ivan", now, now, 100);
-        Employer worker1 = new Employer("Oleg", now, now, 150);
-        Employer worker2 = new Employer("Vadim", now, now, 90);
-        store.add(worker);
-        store.add(worker1);
-        store.add(worker2);
-        ReportEngine engine = new ReportEngine(store);
-        for (Employer employer : store.findBy(em -> true)) {
-            StringBuilder expect = new StringBuilder()
-                    .append("Name; Salary;")
-                    .append(employer.getName()).append(";")
-                    .append(employer.getSalary()).append(";");
-            assertEquals(engine.generateHR(em -> true), (expect.toString()));
-        }
-    }
 }
