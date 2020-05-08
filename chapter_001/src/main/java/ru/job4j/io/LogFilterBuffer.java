7975d3723b2ef06ec847de3d393d4345f49ea391
DmitryF17 <farodmin@yandex.ru>
2020-05-08 14:35:21 +0300

    cleaning code

--- chapter_001/src/main/java/ru/job4j/io/LogFilterBuffer.java
+++ chapter_001/src/main/java/ru/job4j/io/LogFilterBuffer.java
@@ -6,30 +6,36 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
- *
  * @author Dmitry Faroynts (farodmin@yandex.ru).
- *  * version 01.
- *  * @since 01.04.2020.
+ * * version 01.
+ * * @since 01.04.2020.
  */
 public class LogFilterBuffer {
     /**
      * filter for the second from the end.
      *
      * @param file
-     * @return file.
      * @return null.
      */
     public static List<String> filter(String file) {
-        String[] lin = file.split(" ");
-        if (lin[lin.length - 2].equals("404")) {
-            return Collections.singletonList(file);
+        List<String> res = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            while (in.ready()) {
+                String line = in.readLine();
+                String[] lin = line.split(" ");
+                if (lin[lin.length - 2].equals("404")) {
+                    res.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+        return res;
     }
 
     /**
-     *write from a buffer to the file
+     * write from a buffer to the file
+     *
      * @param res
      */
     public static void save(List<String> res) {
@@ -44,25 +50,17 @@ public class LogFilterBuffer {
     }
 
     /**
-     *read the file through a buffer.
+     * read the file through a buffer.
+     * <p>
+     * output filtered elements.
      *
-     *output filtered elements.
      * @param args
      */
     public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
-            List<String> lines = new ArrayList<>();
-            List<String> res = new ArrayList<>();
-            in.lines().forEach(lines::add);
-            for (String line : lines) {
-                List<String> log = filter(line);
-                if (log != null) {
-                   res.add(line);
-                }
-            }
-            save(res);
-        } catch (Exception e) {
-            e.printStackTrace();
+        List<String> log = filter("log.txt");
+        if (!log.isEmpty()) {
+            System.out.println(log);
+            save(log);
         }
     }
 }
