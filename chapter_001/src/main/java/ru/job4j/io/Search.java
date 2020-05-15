51ea7a2ebaebf0ffd503de264f1bd8e65678a129
DmitryF17 <farodmin@yandex.ru>
2020-05-15 17:49:22 +0300

    5. Валидация параметров запуска. [#259756]

--- chapter_001/src/main/java/ru/job4j/io/Search.java
+++ chapter_001/src/main/java/ru/job4j/io/Search.java
@@ -1,22 +1,27 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
-import java.nio.file.Paths;
 import java.util.List;
+
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "txt").forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        List<Path> res = search(start, args[1]);
+        res.stream().map(Path::toString).forEach(System.out::println);
     }
 
-
-    public static List<String> search(Path root, String ext) throws IOException {
-        Files.walkFileTree(root, new PrintFiles(ext));
-        return new PrintFiles(ext).result();
+    public static List<Path> search(Path root, String ext) throws IOException {
+        PrintFiles searcher = new PrintFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
     }
 }
+
+
