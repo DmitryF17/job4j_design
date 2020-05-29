acef58b366e2d92910a5ab049b7d2b5d29dca4b3
DmitryF17 <farodmin@yandex.ru>
2020-05-29 11:44:19 +0300

    3. Slf4j - вывод переменных. [#268852]

--- chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -3,15 +3,26 @@ package ru.job4j.io;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class UsageLog4j {
+
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+
+        String name = "Dmitry";
+
+        byte age = 49;
+        char gender = 'm';
+        short weight = 77;
+        int height = 175;
+        long pulse = 70;
+        float yearinthecourse = 0.5F;
+        double coursedone = 0.33;
+        boolean understandenglish = true;
+
+        LOG.debug("User info name : {}, age : {}, gender : {}, weight : {}, height : {}," +
+                        " understand english : {}, pulse : {}, course done : {}, year in the course : {}",
+                name, age, gender, weight, height, understandenglish, pulse, coursedone, yearinthecourse);
+
     }
 }
