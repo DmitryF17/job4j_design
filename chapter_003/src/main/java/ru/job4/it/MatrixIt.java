a6fb735d0f2cca17c9d0c3ebdf745e1d7cba5bec
DmitryF17 <farodmin@yandex.ru>
2020-06-19 17:38:26 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#259729]

--- chapter_003/src/main/java/ru/job4/it/MatrixIt.java
+++ chapter_003/src/main/java/ru/job4/it/MatrixIt.java
@@ -1,25 +1,42 @@
 package ru.job4.it;
-
+/**
+ * Package for iterators.
+ * author DmitryF17
+ * version 01
+ * since 17/06/2019
+ */
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
+/**
+ *  An iterator for a two-dimensional array
+ *  author DmitryF17
+ *  version 02
+ *  since 19/06/2019
+ */
 public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
     private int row = 0;
     private int column = 0;
-    private int point = 0;
 
     public MatrixIt(int[][] data) {
         this.data = data;
     }
 
+    /**
+     * method where elements are checked in each nested array
+     * @return boolean
+     */
     @Override
     public boolean hasNext() {
-        int allelement = 0;
-        for (int[] dat : data) {
-            allelement += dat.length;
+
+        if (column == data[row].length & row != data.length - 1) {
+            row++;
+            column = 0;
         }
-        return point < allelement;
+        while (data[row].length == 0 & row != data.length - 1) {
+            row++;
+        }
+        return data[row].length > column;
     }
 
     @Override
@@ -27,14 +44,6 @@ public class MatrixIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        if (column == data[row].length) {
-            row++;
-            column = 0;
-        }
-        while (data[row].length == 0) {
-            row++;
-        }
-        point++;
         return data[row][column++];
     }
 }
