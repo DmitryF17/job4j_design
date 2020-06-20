0f005584316abd7da7ce5101b7605c2df035382a
DmitryF17 <farodmin@yandex.ru>
2020-06-20 23:00:27 +0300

    5.1.2. Создать итератор четные числа [#259726]

--- chapter_003/src/main/java/ru/job4/it/EvenIterator.java
+++ chapter_003/src/main/java/ru/job4/it/EvenIterator.java
@@ -1,39 +1,43 @@
 package ru.job4.it;
-
+/**
+ * Package for iterators.
+ * author DmitryF17
+ * version 01
+ * since 17/06/2019
+ */
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+/**
+ *  An iterator for a even numbers
+ *  author DmitryF17
+ *  version 03
+ *  since 20/06/2019
+ */
 
 public class EvenIterator implements Iterator<Integer> {
     private final int[] numbers;
     private int point = 0;
-    private int[] evennum;
 
     public EvenIterator(int[] numbers) {
         this.numbers = numbers;
-        this.evennum = haseven();
     }
-
-    private int[] haseven() {
-        evennum = new int[]{};
-        int index = 0;
-        for (int even : numbers) {
-            if (even % 2 == 0) {
-                evennum[index++] = even;
-            }
-        }
-        return evennum;
-    }
-
+    /**
+     * a method where even number create index for array and check end of array
+     * @return boolean
+     */
     @Override
     public boolean hasNext() {
-        return point < evennum.length;
-    }
 
+        while (numbers[point] % 2 != 0 & point < numbers.length - 1) {
+            point++;
+        }
+        return numbers[point] % 2 == 0;
+    }
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return evennum[point++];
+        return numbers[point++];
     }
 }
