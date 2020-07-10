0296c19ba361260478ff5165b2edd12f30e75d9a
DmitryF17 <farodmin@yandex.ru>
2020-07-10 15:29:07 +0300

    2. Не перекрывать equals hashCode [#259742]

--- chapter_003/src/main/java/ru/job4/collection/User.java
+++ chapter_003/src/main/java/ru/job4/collection/User.java
@@ -1,6 +1,9 @@
 package ru.job4.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +14,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] arge) {
+        User first = new User("first", 1, new GregorianCalendar(2000, Calendar.JUNE, 23));
+        User second = new User("first", 1, new GregorianCalendar(2000, Calendar.JUNE, 23));
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(second, "second");
+        System.out.println(map);
     }
 }
