21a98d1d2c7ad6b5b75236dac653ff9fa4649f47
DmitryF17 <farodmin@yandex.ru>
2020-07-12 15:57:29 +0300

    5. Перекрывать и equals и hashCode [#259739]

--- chapter_003/src/main/java/ru/job4/collection/User.java
+++ chapter_003/src/main/java/ru/job4/collection/User.java
@@ -7,10 +7,18 @@ public class User {
     private int children;
     private Calendar birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    private User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + children;
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
     }
 
     @Override
@@ -21,8 +29,8 @@ public class User {
         User user = (User) o;
 
         if (children != user.children) return false;
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
-        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+        if (!Objects.equals(name, user.name)) return false;
+        return Objects.equals(birthday, user.birthday);
     }
 
 
