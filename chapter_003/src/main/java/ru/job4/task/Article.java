420022c04e403ef2c532ab5428de5858ea26d8fc
DmitryF17 <farodmin@yandex.ru>
2020-07-31 21:37:09 +0300

    Упражнения 2.1.5 HashMap

---
+++ chapter_003/src/main/java/ru/job4/task/Article.java
@@ -0,0 +1,15 @@
+package ru.job4.task;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        boolean res = true;
+        String[] newtext = line.split(" ");
+        for (String word : newtext) {
+            if (!origin.contains(word)) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+}
