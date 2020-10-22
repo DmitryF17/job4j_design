eed12a8e3f2f72434014915a448ea1592b3f543f
DmitryF17 <farodmin@yandex.ru>
2020-10-22 11:09:28 +0300

    3. Парсер вакансий на sql.ru [#259700]

--- chapter_004/agregator.sql
+++ chapter_004/agregator.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post (
 id serial PRIMARY KEY,
-name varchar UNIQUE NOT NULL,
+name varchar ,
 text text,
-link text,
+link text UNIQUE NOT NULL,
 created date NOT NULL DEFAULT current_date
 );
